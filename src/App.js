96c05806e8ec75bd9fd4f396e187cd79fff108c2
Gigi Minova <minova.gigi@gmail.com>
2019-09-24 14:23:55 +0100

    set up first chart

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import AreaChart from './Components/AreaChart';
+
+function App() {
+  return (
+    <div className="App">
+      <AreaChart></AreaChart>
+    </div>
+  );
+}
+
+export default App;
